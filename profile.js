6bdd617fc4c90b95ce2f0edd162d9b547ba6f157
victoria Emmanuel <linkontobridget@gmail.com>
2025-07-17 09:08:37 -0700

    initial commit

---
+++ profile.js
@@ -0,0 +1,9 @@
+function getStarted() {
+    alert("Hello Victoria! Welcome to your personal portfolio.");
+}
+
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+    body.classList.toggle("light-mode");
+}
